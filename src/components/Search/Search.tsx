8aa7927f56b0e6a7adeeb9b73691a815f05a3d62
Adrian Pilarczyk <adrian@useo.pl>
2021-02-04 19:24:17 +0100

    change: Search to use useSearch hook

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,30 @@
-const Search = () => (
-  <form>
-    <input
-      type="text"
-      onClick={() => console.log("click")}
-      value=""
-      placeholder="Enter text"
-    />
-    <button type="submit">Submit</button>
-  </form>
-);
+import { useState } from "react";
+import useSearch from "../../hooks/useSearch";
+import Results from "../Results/Results";
+
+const Search: React.FC = () => {
+  const [value, setValue] = useState("");
+  const { results, isError, isLoading } = useSearch(value);
+
+  const isResponseEmpty = value !== "" && results?.Response === "False";
+
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          placeholder="Enter text"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {isLoading && <h2>Loading...</h2>}
+      {isError && <h2>Woops!</h2>}
+      {isResponseEmpty && <h2>No data :(</h2>}
+      {results?.Search && <Results results={results.Search} />}
+    </div>
+  );
+};
 
 export default Search;
