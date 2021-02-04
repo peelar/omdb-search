0417e779b9961901641ece61e4581c0cb0e42bd2
Adrian Pilarczyk <adrian@useo.pl>
2021-02-04 20:34:06 +0100

    add: to-do item for tomorrow

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,6 +1,7 @@
 import { SearchEntry } from "../../../types";
 
 const Results: React.FC<{ results: SearchEntry[] }> = ({ results }) => {
+  // to-do: pimp up Results
   return (
     <ul>
       {results.map((result, index) => (
