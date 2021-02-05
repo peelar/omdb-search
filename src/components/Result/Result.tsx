d415aa67490846b5e1b594563289c7d566927359
Adrian Pilarczyk <adrian@useo.pl>
2021-02-05 17:05:41 +0100

    change: extract Card from Result

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -8,28 +8,40 @@ import {
 } from "@material-ui/core";
 import { SearchEntry } from "../../../types";
 
-const Result: React.FC<SearchEntry> = (result) => {
-  return (
-    <Card>
-      <CardActionArea>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <CardMedia
-              component="img"
-              height="200px"
-              src={result.Poster}
-              title={result.Title}
-            />
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <CardContent>
-              <Typography variant="h4">{`${result.Title} [${result.Year}]`}</Typography>
-            </CardContent>
-          </Grid>
+const ResultCard: React.FC<{
+  media: React.ReactNode;
+  content: React.ReactNode;
+}> = ({ media, content }) => (
+  <Card>
+    <CardActionArea>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          {media}
         </Grid>
-      </CardActionArea>
-    </Card>
-  );
-};
+        <Grid item xs={12} md={8}>
+          {content}
+        </Grid>
+      </Grid>
+    </CardActionArea>
+  </Card>
+);
+
+const Result: React.FC<SearchEntry> = (result) => (
+  <ResultCard
+    media={
+      <CardMedia
+        component="img"
+        height="200px"
+        src={result.Poster}
+        title={result.Title}
+      />
+    }
+    content={
+      <CardContent>
+        <Typography variant="h4">{`${result.Title} [${result.Year}]`}</Typography>
+      </CardContent>
+    }
+  />
+);
 
 export default Result;
