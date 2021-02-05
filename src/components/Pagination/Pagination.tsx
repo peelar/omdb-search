8348dce9994e9b23a24f4436d74ae884b6abcaa9
Adrian Pilarczyk <adrian@useo.pl>
2021-02-05 17:41:46 +0100

    change: move to top when pagination clicked

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -11,12 +11,18 @@ type PaginationProps = {
 const Pagination: React.FC<PaginationProps> = ({ total, page, setPage }) => {
   // the "+" sign is a trick so TypeScript leaves me alone
   const count = Math.round(+total / RESULTS_PER_PAGE);
+
+  const handleChange = (value: number) => {
+    window.scrollTo(0, 0);
+    setPage(value);
+  };
+
   return (
     <nav>
       <MUIPagination
         page={page}
         count={count}
-        onChange={(_, value) => setPage(value)}
+        onChange={(_, value) => handleChange(value)}
       />
     </nav>
   );
